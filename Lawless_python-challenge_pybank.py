09d104519fb526efb0da3aaa09ac7ef80ed3996f
TJ Lawless <87883465+tommyjohnny36@users.noreply.github.com>
2021-10-02 22:22:29 -0400

    Cleaned up script
    Cleaned the script up and made my comments

--- Lawless_python-challenge_pybank.py
+++ Lawless_python-challenge_pybank.py
@@ -1,3 +1,15 @@
+#  Your task is to create a Python script that analyzes the records to calculate each of the following:    
+ 
+#  The total number of months included in the dataset    
+ 
+#  The net total amount of "Profit/Losses" over the entire period    
+ 
+#  Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes    
+ 
+#  The greatest increase in profits (date and amount) over the entire period    
+ 
+#  The greatest decrease in profits (date and amount) over the entire period 
+
 import csv
 
 # Define file path for program to go get data
@@ -6,7 +18,6 @@ csvpath = r"E:\Georgia Tech\02-Homework\03-Python\python-challenge\Instructions\
 total_months = []
 total_profit = []
 monthly_change = []
-
 
 
 with open(csvpath, 'r') as csvfile:
